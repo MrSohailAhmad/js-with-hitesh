cf56d02afce1a6130bddf1ba500d2697758f9818
MrSohailAhmad <ahmadsohail1819@gmail.com>
2024-10-13 11:53:08 +0500

    learn conversion operation

---
+++ basic/conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "30";
+// let score = "123abc";
+// let score = null;
+// let score = undefined;
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score); // here we change string into number
+
+// console.log(valueInNumber); // now it type is number
+
+// ! Notes
+
+// ? we convert string "33" into number 33 then its easily convert in number
+
+// ? when we convert "123abc" into number then its return NaN
+
+// ? when we convert null into number and its return 0 (zero)
+
+// ? when we convert undefined into number its return NaN
+
+// let isLoggedIn = 0;
+let isLoggedIn = 1; // when we pass other then 0 zero number then its return true after conversion into boolean
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);

cebc49bb1c4280835a9af6179951800dfa23f2b2
MrSohailAhmad <ahmadsohail1819@gmail.com>
2024-10-13 10:18:05 +0500

    basic variables

---
+++ basic/variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail = "ahmadsohail1819@gmail.com";
+var accountPassword = "12345";
+
+// accountCity == "kohat"; // not allowed
+
+/*
+prefer not to use var
+
+because of issue in block scope and functional scope 
+*/
+
+console.table({ accountId, accountEmail, accountPassword });

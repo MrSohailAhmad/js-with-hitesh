f28a1e304b132f18423043a9430ee0868f394087
MrSohailAhmad <ahmadsohail1819@gmail.com>
2024-10-13 10:17:51 +0500

    data types of js

---
+++ basic/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs not a browser
+
+//  ? some data types in js
+
+/*
+? this is a premitive datatypes
+number   2 to power 53
+bigint
+
+string is wrape in double, single and back ticks
+
+boolean   true and false   true / false
+
+null is a standalone value 
+
+undefined 
+
+symbol   use for uniqueness
+
+
+
+? non premitive
+
+object 
+
+
+typeof  keyword we use to check types of variables
+
+*/
+console.log(typeof undefined);
